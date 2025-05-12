2fbeb5ec72dacbcf3f68c038cf75f44e5167e2ac
manffred-calvosanchez_data <manffred.calvosanchez@databricks.com>
2025-05-11 21:45:13 -0600

    Adding skeleton of account tools

---
+++ telco_support_agent/tools/account_agent_tools.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+from unitycatalog.ai.core.databricks import DatabricksFunctionClient
+from unitycatalog.ai.openai.toolkit import ChatCompletionToolParam, UCFunctionToolkit
+
+from telco_support_agent.tools import ToolInfo
+
+
+class UCTool(ABC):
+    """Base UC Tool class all tools will inherit from."""
+
+    def __init__(self, function_name: str):
+        self.client = DatabricksFunctionClient()
+        self.function_name = function_name
+        self.spec = self.create_function()
+        self.exec_fn = self.client.get_function_as_callable(
+            function_name=self.function_name
+        )
+
+    @abstractmethod
+    def create_function(self) -> ChatCompletionToolParam:
+        """Create tool function in unity catalog."""
+        pass
+
+    def get_tool_info(self) -> ToolInfo:
+        """Return tool info to an agent inherent from BaseAgent."""
+        return ToolInfo(
+            function_name=self.function_name, spec=self.spec, exec_fn=self.exec_fn
+        )
+
+
+class AccountInfoTool(UCTool):
+    def __init__(self) -> None:
+        super().__init__("account_info_tool")
+
+    def create_function(self) -> ChatCompletionToolParam:
+        """Create tool function in unity catalog."""
+        self.client.create_function(sql_function_body="")
+        toolkit = UCFunctionToolkit(function_names=[self.function_name])
+        return toolkit.tools[0]
