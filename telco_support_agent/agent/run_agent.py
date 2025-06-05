f1e1cd990c67eda137524bcd45f46de6fcf7ed81
Sid Murching <sid.murching@databricks.com>
2025-06-04 23:26:35 -0700

    Revert "WIP trying ResponsesAgent, will switch back to ChatAgent for simplicity"
    This reverts commit 7d7e302b8234fadd996b88b35026c221de937061.

--- telco_support_agent/agent/run_agent.py
+++ telco_support_agent/agent/run_agent.py
@@ -5,10 +5,8 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.WARNING)
     # user_query = "What was my bill for the last few months (my customer ID is CUS-10001)?"
     user_query = "Is there an outage in Moscone center right now?"
-    for event in AGENT.predict_stream({"messages": [{"role": "user", "content": user_query}]}):
-        print(event)
 
-    # for chunk in AGENT.predict_stream({"messages": [{"role": "user", "content": user_query}]}):
-    #     if chunk.delta.tool_calls:
-    #         print(chunk.delta.tool_calls)
-    #     print(chunk.delta.content)
+    for chunk in AGENT.predict_stream({"messages": [{"role": "user", "content": user_query}]}):
+        if chunk.delta.tool_calls:
+            print(chunk.delta.tool_calls)
+        print(chunk.delta.content)
